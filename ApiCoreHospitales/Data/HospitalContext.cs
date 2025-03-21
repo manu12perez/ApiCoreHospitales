10b5fb3615b500678fb82eecc5ec330a64551529
manu12perez <manu12perez@gmail.com>
2025-03-21 11:43:18 +0100

    Add project files.

---
+++ ApiCoreHospitales/Data/HospitalContext.cs
@@ -0,0 +1,12 @@
+﻿using ApiCoreHospitales.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCoreHospitales.Data
+{
+    public class HospitalContext: DbContext
+    {
+        public HospitalContext(DbContextOptions<HospitalContext> options) : base(options) { }
+
+        public DbSet<Hospital> Hospitales { get; set; }
+    }
+}
---
+++ ApiCoreHospitales/Program.cs
@@ -0,0 +1,45 @@
+using ApiCoreHospitales.Data;
+using ApiCoreHospitales.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+/****************************************************************************************************/
+// Configuración de SQL Server
+string connectionString = builder.Configuration.GetConnectionString("SqlAzure");
+builder.Services.AddDbContext<HospitalContext>(options => options.UseSqlServer(connectionString));
+
+// Registro de repositorios
+builder.Services.AddTransient<RepositoryHospitales>();
+/****************************************************************************************************/
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    /*
+     *  app.MapOpenApi();
+     */    
+}
+/****************************************************************************************************/
+app.MapOpenApi();
+app.UseSwaggerUI(
+    options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Api Hospitales 2025");
+        options.RoutePrefix = "";
+});
+/****************************************************************************************************/
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
